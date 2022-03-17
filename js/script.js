33b6296d972c0a19d855c22ebb1983a2130137f4
PamLeles <pamela_leles_sousa@outlook.com>
2022-03-17 17:04:21 -0300

    colisão jogador/inimigo1

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ function start() {
 	$("#fundoGame").append("<div id='amigo' class='anima3'></div>");
    
 var jogo = {}
+var podeAtirar=true;
 var velocidade=5;
 var posicaoY = parseInt(Math.random() * 334);
 var TECLA = {
@@ -16,7 +17,7 @@ var TECLA = {
 	D: 68
 	}
 
-	jogo.pressionou = [];
+jogo.pressionou = [];
     $(document).keydown(function(e){
         jogo.pressionou[e.which] = true;
         });
@@ -34,6 +35,7 @@ function loop() {
     moveinimigo1();
     moveinimigo2();
     moveamigo();
+	colisao();
 } 
 
 function movefundo() {
@@ -65,8 +67,7 @@ function movejogador() {
 	}
 	
 	if (jogo.pressionou[TECLA.D]) {
-		
-		//Chama fun��o Disparo	
+		disparo();
 	}
 
 	} 
@@ -107,6 +108,82 @@ function moveamigo() {
 		}
 
 } 
+
+function disparo() {
+	
+	if (podeAtirar==true) {
+		
+	podeAtirar=false;
+	
+	topo = parseInt($("#jogador").css("top"))
+	posicaoX= parseInt($("#jogador").css("left"))
+	tiroX = posicaoX + 190;
+	topoTiro=topo+40;
+	$("#fundoGame").append("<div id='disparo'></div");
+	$("#disparo").css("top",topoTiro);
+	$("#disparo").css("left",tiroX);
+	
+	var tempoDisparo=window.setInterval(executaDisparo, 30);
+	
+	} 
+ 
+   	    function executaDisparo() {
+	    posicaoX = parseInt($("#disparo").css("left"));
+	    $("#disparo").css("left",posicaoX+15); 
+
+        		if (posicaoX>900) {
+						
+			window.clearInterval(tempoDisparo);
+			tempoDisparo=null;
+			$("#disparo").remove();
+			podeAtirar=true;
+					
+     	}
+	} 
+} 
+
+
+function colisao() {
+	var colisao1 = ($("#jogador").collision($("#inimigo1")));
+	
+		if (colisao1.length>0) {
+			
+		inimigo1X = parseInt($("#inimigo1").css("left"));
+		inimigo1Y = parseInt($("#inimigo1").css("top"));
+		explosao1(inimigo1X,inimigo1Y);
+	
+		posicaoY = parseInt(Math.random() * 334);
+		$("#inimigo1").css("left",694);
+		$("#inimigo1").css("top",posicaoY);
+		}
+	
+	} 
+
+function explosao1(inimigo1X,inimigo1Y) {
+	$("#fundoGame").append("<div id='explosao1'></div");
+	$("#explosao1").css("background-image", "url(./assets/imgs/explosao.png)");
+	var div=$("#explosao1");
+	div.css("top", inimigo1Y);
+	div.css("left", inimigo1X);
+	div.animate({width:200, opacity:0}, "slow");
+	
+	var tempoExplosao=window.setInterval(removeExplosao, 1000);
+	
+		function removeExplosao() {
+			
+			div.remove();
+			window.clearInterval(tempoExplosao);
+			tempoExplosao=null;
+			
+		}
+		
+	} 
+
+	
+
+
+
+
 
 
 } 
